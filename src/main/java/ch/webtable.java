e8841d888be014d665319229e6ad073c32049dcd
indira palaniappan <indirapalaniappan07@gmail.com>
2023-06-20 13:50:36 +0530

    new commit from indira

--- src/main/java/ch/webtable.java
+++ src/main/java/ch/webtable.java
@@ -38,83 +38,69 @@ import org.openqa.selenium.support.ui.WebDriverWait;
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class webtable {
-	
-	static int htext ;
-	static int cvalue ;
-	
 
-	public static void main(String[] args)   {
-		
-		
-	try {	
-WebDriverManager.chromedriver().setup();
-		
-		WebDriver driver =new ChromeDriver();
-	
-		
-	WebDriverWait w = new WebDriverWait(driver, 10);
-		driver.get("https://qavbox.github.io/demo/webtable/");
-		driver.manage().window().maximize();
-		
-	int rsize = driver.findElements(By.xpath("//table[@id='table02']//tbody//tr")).size();
-	int csize = driver.findElements(By.xpath("//table[@id='table02']//tr[1]//td")).size();	
-	
-	System.out.println(rsize);
-	System.out.println(csize);	
-	
-	File f = new File("/Users/gowtham/eclipse-workspace/ch/src/main/resources/table3.xlsx");
-	Workbook wb = new XSSFWorkbook();
-	Sheet sheet = wb.createSheet();
-	
-	List<WebElement> list = driver.findElements(By.xpath("//table[@id='table02']//thead//tr//th"));
-	int head = list.size();
-	System.out.println("h size "+head);
-	
-	
-	for (WebElement hlist : list) {
-		
-		String htext = hlist.getText();
-		System.out.println(htext);
-	      Row ro = sheet.createRow(0);
-	      
-	
-			
-			
-		
-		
-		
-	}
-	
-		for (int i = 1; i <= rsize; i++) {
-			Row row = sheet.createRow(i);
-			
-			for (int j = 1; j <=csize; j++) {
-				WebElement element = driver.findElement(By.xpath("//table[@id='table02']//tbody//tr["+ i +"]//td["+ j+ "]"));
-				
-				String text = element.getText();
-				Cell hcell = row.createCell(j);	
-				hcell.setCellValue(htext);
-				Cell cell = row.createCell(j);
-				cell.setCellValue(text);
-				System.out.print(text +"     \t");
+	static int htext;
+	static int cvalue;
+
+	public static void main(String[] args) {
+
+		try {
+			WebDriverManager.chromedriver().setup();
+
+			WebDriver driver = new ChromeDriver();
+
+			WebDriverWait w = new WebDriverWait(driver, 10);
+			driver.get("https://qavbox.github.io/demo/webtable/");
+			driver.manage().window().maximize();
+
+			int rsize = driver.findElements(By.xpath("//table[@id='table02']//tbody//tr")).size();
+			int csize = driver.findElements(By.xpath("//table[@id='table02']//tr[1]//td")).size();
+
+			System.out.println(rsize);
+			System.out.println(csize);
+
+			File f = new File("/Users/gowtham/eclipse-workspace/ch/src/main/resources/table3.xlsx");
+			Workbook wb = new XSSFWorkbook();
+			Sheet sheet = wb.createSheet();
+
+			List<WebElement> list = driver.findElements(By.xpath("//table[@id='table02']//thead//tr//th"));
+			int head = list.size();
+			System.out.println("h size " + head);
+
+			for (WebElement hlist : list) {
+
+				String htext = hlist.getText();
+				System.out.println(htext);
+				Row ro = sheet.createRow(0);
+
 			}
-			System.out.println();
+
+			for (int i = 1; i <= rsize; i++) {
+				Row row = sheet.createRow(i);
+
+				for (int j = 1; j <= csize; j++) {
+					WebElement element = driver
+							.findElement(By.xpath("//table[@id='table02']//tbody//tr[" + i + "]//td[" + j + "]"));
+
+					String text = element.getText();
+					Cell hcell = row.createCell(j);
+					hcell.setCellValue(htext);
+					Cell cell = row.createCell(j);
+					cell.setCellValue(text);
+					System.out.print(text + "     \t");
+				}
+				System.out.println();
+			}
+
+			FileOutputStream f2 = new FileOutputStream(f);
+
+			wb.write(f2);
+			f2.close();
+
+		} catch (Exception e) {
+			System.out.println(e.toString());
 		}
-		
-	
-		FileOutputStream f2 = new FileOutputStream(f);
-		
-		wb.write(f2);
-		f2.close();	
-		
-		
-		
+
 	}
-	catch (Exception e) {
-		System.out.println(e.toString());
-	}
-	
-	}
-	
 
 }
